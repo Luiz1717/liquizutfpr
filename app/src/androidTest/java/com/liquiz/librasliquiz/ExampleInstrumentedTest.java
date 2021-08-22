8e9edb5c71743e686c224500f389a292a4bb31cb
luiz <luizw@alunos.utfpr.edu.br>
2021-08-22 12:58:29 -0300

    LIQUIZ

--- app/src/androidTest/java/com/liquiz/librasliquiz/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/liquiz/librasliquiz/ExampleInstrumentedTest.java
@@ -2,8 +2,8 @@ package com.liquiz.librasliquiz;
 
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,8 +15,9 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
 @RunWith(AndroidJUnit4.class)
-public class liquizInstrumentedTest {
+public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
@@ -24,3 +25,5 @@ public class liquizInstrumentedTest {
         assertEquals("com.liquiz.librasliquiz", appContext.getPackageName());
     }
 }
+
+
